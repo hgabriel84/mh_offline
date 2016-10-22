4180b379d57d1971dbe50ef608357e7e61981ed7
Hugo Gabriel <hgabriel@premium-minds.com>
2016-10-22 18:25:15 +0100

    added images for background play / stop videos logic added to javascript file added video player pages videos are not upload via git push, it was taking too long

--- js/script.js
+++ js/script.js
@@ -1,9 +1,34 @@
+var page = $('.page');
+var home = $('#home');
+var vd_eplanning = $('#vd_eplanning');
+var vd_weekend = $('#vd_weekend');
+var pagename_eplanning = 'eplanning';
+var pagename_weekend_video = 'weekend_video';
+
 $(function() {
-	$('.page').hide();
-	$('#home').show();
+	page.hide();
+	home.show();
 });
 
 function show(id) {
-	$('.page').hide();
+	page.hide();
 	$('#' + id).show();
+
+  stopVideos();
+
+  if(id == pagename_eplanning) {
+    startVideo(vd_eplanning);
+  }
+
+  if(id == pagename_weekend_video) {
+    startVideo(vd_weekend);
+  }
+}
+
+function startVideo(vd) {
+  vd[0].play();
+}
+
+function stopVideos() {
+  $('video').each(function () { this.pause(); this.currentTime = 0; });
 }
