b10777cf102e0234341992258d02c4f9061d1628
Hugo Gabriel <hgabriel@premium-minds.com>
2016-10-23 21:38:53 +0100

    cannot download file as csv. will only display it.

--- js/script.js
+++ js/script.js
@@ -172,8 +172,6 @@ function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
   var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
   var CSV = '';
 
-  CSV += ReportTitle + '\r\n\n';
-
   if (ShowLabel) {
     var row = "";
     for (var index in arrData[0]) {
@@ -197,18 +195,7 @@ function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
     return;
   }
 
-  //var fileName = ReportTitle;
-  //var uri = 'data:application/octet;charset=utf-8,' + escape(CSV);
   window.open('data:text/csv;charset=utf-8,' + escape(CSV));
-  //window.location.href = uri;
-  
-  // var link = document.createElement("a");
-  // link.href = uri;
-  // //link.style = "visibility:hidden";
-  // link.download = fileName + ".csv";
-  // document.body.appendChild(link);
-  // link.click();
-  // document.body.removeChild(link);
 }
 
 function getRandomInt(min, max) {
